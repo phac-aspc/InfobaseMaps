5cd375f69be9ecba6920b236e3ba647f3caeec7b
JohicMes <johicmes@gmail.com>
2021-03-02 15:48:35 -0500

    Created Map and data for new look

--- src/js/ProvTerJs/ProvincesTerritories.js
+++ src/js/ProvTerJs/ProvincesTerritories.js
@@ -6,26 +6,30 @@ isIE = true;
 
 // This section creates a color scale. A function could be created to do this.
 var COLOR_SCALE = {
-  "4.188 to 6.395": "#d7191c",
-  "2.197 to 4.187": "#fdae61",
-  "0.950 to 2.196": "#ffffbf",
-  "0.315 to 0.949": "#abd9e9",
-  "0.003 to 0.314": "#2c7bb6"
+    "80 to 100" : "#1c7d6c",
+    "60 to 79" : "#29bca1",
+    "40 to 59" : "#58dac2",
+    "20 to 39" : "#96e8d9",
+    "0 to 19" : "#eafaf7",
+    "No data" : "#ffffff"
 };
 
 // D3 fetches the data
-d3.csv("../src/data/CensusRegionsData/data.csv", function(error, data) {
+d3.csv("../src/data/ProvData/data.csv", function(error, data) {
 
   if (error) throw error;
 
   // format the data
   data.forEach(function(d) {
-    d.percent = +d["Rate(per 100,000)"];
-    d.HR = +d.CD;
+    d.year = +d["year"]
+    d.month = +d["month"]
+    d.hirisk = +d["hirisk"]
+    d.age = +d["age"]
+    d.PRUID = +d.PRUID;
   });
 
   // D3 fetches the topojson to create the map.
-  d3.json('../src/topojson/CensusMap.json', function(mapJSON) {
+  d3.json('../src/topojson/ProvincesTerritoriesMap.json', function(mapJSON) {
     svg = d3.selectAll('#map')
     .append('svg')
     .attr("id", "svg")
@@ -38,31 +42,31 @@ d3.csv("../src/data/CensusRegionsData/data.csv", function(error, data) {
     .attr("preserveAspectRatio","xMinYMin meet")
     .attr("viewBox","0 0 900 800"); // Fix up the SVG attributes to fit and look how we want
 
-    var healthregions = topojson.feature(mapJSON, mapJSON.objects.wildfireMap);
+    var provinces = topojson.feature(mapJSON, mapJSON.objects.Can_PR2016);
 
     var projection = d3.geoIdentity((function(x, y) {
       return [2*x, y]; // Create the projection
     }))
-    .reflectY(true) // Add some attributes that ensure it fist teh viewbox
-    .fitExtent([[0, 0], [880, 880]], healthregions);
+    .reflectY(true) // Add some attributes that ensure it fist the viewbox
+    .fitExtent([[0, 0], [880, 880]], provinces);
 
     // Create SVG elements then bind data to them.
     var path = d3.geoPath().projection(projection);
 
-    var hrid = svg
+    var prid = svg
     .append('g')
     .attr("id", "mapGroup")
     .attr("transform","translate(0,-50)")
     .selectAll('g')
-    .data(healthregions.features)
+    .data(provinces.features)
     .enter()
 
-    hrid
+    prid
     .append("g")
     .attr("focusable","true")
     .append('path')
-    .attr("id", function(d) { return d.properties["CDUID"]})
-    .attr("class", "CDUID")
+    .attr("id", function(d) { return d.properties["PRUID"]})
+    .attr("class", "PRUID")
     .attr("d", path)
     .attr("stroke","#333")
     .attr("stroke-width",1);
@@ -96,18 +100,25 @@ d3.csv("../src/data/CensusRegionsData/data.csv", function(error, data) {
         d3.select(this).select("path").attr("stroke-width",3);
       }
 
-      document.getElementById("textarea").innerHTML =
-      "<p>This map shows X rate of Y in Canada"
-      +"<p>The X rate (per 100,000) attributed to Y in the <strong>"
-      + d.properties.CDNAME + "</strong> census district in 2018 was <strong>"
-      + d.percent
-      + "%</strong>.</p>";
+      // Update the text
+      if (d.age != undefined){
+        document.getElementById("textarea").innerHTML =
+        "<p>The mean age for X in <strong>"
+        + d.properties.PRENAME + "</strong> in 2018 was <strong>"
+        + d.age
+        + "</strong>.</p>";
+      } else {
+        document.getElementById("textarea").innerHTML =
+        "<p>No data available for "
+        + d.properties.PRENAME + "."
+      }
+
     })
+
     // Do the same things as on hover but when a Health Region is tabbed over (Accessibility)
     .on("focus", function(d){
       if(!d3.select(this).select("path").classed("activeRegion")){
         d3.select(this).select("path").attr("data-fill",d3.select(this).select("path").attr("fill"));
-        console.log("focus oldColour is : ",oldColour);
 
         var t = textures
         .lines()
@@ -124,12 +135,19 @@ d3.csv("../src/data/CensusRegionsData/data.csv", function(error, data) {
         d3.select(this).select("path").attr("stroke-width",3);
       }
 
-      document.getElementById("textarea").innerHTML =
-      "<p>This map shows X rate of Y in Canada"
-      +"<p>The X rate (per 100,000) attributed to Y in the <strong>"
-      + d.properties.CDNAME + "</strong> census district in 2018 was <strong>"
-      + d.percent
-      + "%</strong>.</p>";
+      // Update the text
+      if (d.age != undefined){
+        document.getElementById("textarea").innerHTML =
+        "<p>The mean age for X in <strong>"
+        + d.properties.PRENAME + "</strong> in 2018 was <strong>"
+        + d.age
+        + "</strong>.</p>";
+      } else {
+        document.getElementById("textarea").innerHTML =
+        "<p>No data available for "
+        + d.properties.PRENAME + "."
+      }
+
     })
     //Whean leaving a shape return it to normal
     .on("mouseout", function(d){
@@ -150,30 +168,33 @@ d3.csv("../src/data/CensusRegionsData/data.csv", function(error, data) {
 
   // Colours in all the census regions of the map
   function colorMap(){
-    d3.selectAll(".CDUID")
+    d3.selectAll(".PRUID")
     .attr("fill", function(regions) {
-      var HR = regions.properties["CDUID"];
+      var PRUID = regions.properties["PRUID"];
       var i = 0;
       while (i < data.length){
-        if (data[i]["HR"] == HR){
-          regions.percent = data[i].percent;
-          return color(data[i].percent)
+        var c;
+        if (data[i]["PRUID"] == PRUID){
+          regions.age = data[i].age;
+          c = color(data[i].age)
+          break;
         } else {
-
+          c = COLOR_SCALE["No data"];
         }
         i ++
       }
-
+      return c;
     });
   }
 
   // Return a color value depending on the value of the data
   function color(value){
-    if (value >= 4.188) return COLOR_SCALE["4.188 to 6.395"];
-    else if (value >= 2.197) return COLOR_SCALE["2.197 to 4.187"];
-    else if (value >= 0.950) return COLOR_SCALE["0.950 to 2.196"];
-    else if (value >= 0.315) return COLOR_SCALE["0.315 to 0.949"];
-    else return COLOR_SCALE["0.003 to 0.314"];
+    if (value >= 13.7) return COLOR_SCALE["80 to 100"];
+                else if (value >= 13.5) return COLOR_SCALE["60 to 79"];
+                else if (value >= 13.3) return COLOR_SCALE["40 to 59"];
+                else if (value >= 13.1) return COLOR_SCALE["20 to 39"];
+                else if (value >= 12.9) return COLOR_SCALE["0 to 19"];
+                else return COLOR_SCALE["No data"];
   }
 
   // As the function name says... Creates and appends a Legend to the SVG
@@ -217,7 +238,7 @@ d3.csv("../src/data/CensusRegionsData/data.csv", function(error, data) {
     .attr("font-weight","bold")
     .append("tspan")
     .attr("y",topMargin-20)
-    .text("X rate (per 100,000)");
+    .text("Mean Age");
 
     // Appends text to all the categories (rectangles) of the legend
     d3.selectAll('.category')
@@ -234,15 +255,15 @@ d3.csv("../src/data/CensusRegionsData/data.csv", function(error, data) {
     .style("text-anchor","end")
     .text(function(d,i){
         if(i==0){
-          return "4.188 to 6.395";
+          return "13.5 < ";
         }else if(i==1){
-          return "2.197 to 4.187";
+          return "13.5 >=";
         }else if(i==2){
-          return "0.950 to 2.196";
+          return "13.3 >=";
         }else if(i==3){
-          return "0.315 to 0.949"
+          return "13.1 >="
         }else if(i==4){
-          return "0.003 to 0.314";
+          return "12.9 >=";
         }
     });
   }
